561985a5b526615dd0c37fb30259b12a753b49f3
Jolivan Abreu <jolivan489@gmail.com>
2025-09-18 15:02:17 -0300

    Update

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -1,12 +1,11 @@
-// frontend/tailwind.config.js
 /** @type {import('tailwindcss').Config} */
-export default {
-    content: [
-        './index.html',
-        './src/**/*.{js,ts,jsx,tsx}', // Garanta que esta linha está correta
-    ],
-    theme: {
-        extend: {},
-    },
-    plugins: [],
+module.exports = {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
 }
