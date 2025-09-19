e04828a6238783e16f11b4ec05c4b6b9d0c8875c
Jolivan Abreu <jolivan489@gmail.com>
2025-09-19 12:38:38 -0300

    Update

--- backend/src/config/database.js
+++ backend/src/config/database.js
@@ -1,10 +1,8 @@
 // backend/src/config/database.js
-
 const path = require('path');
 require('dotenv').config({ path: path.resolve(__dirname, '..', '..', '.env') });
 
-// Configuração para o ambiente de desenvolvimento (o seu computador)
-const developmentConfig = {
+module.exports = {
   dialect: 'mariadb',
   host: process.env.DB_HOST,
   username: process.env.DB_USER,
@@ -16,22 +14,3 @@ const developmentConfig = {
     underscoredAll: true,
   },
 };
-
-// Configuração para o ambiente de produção (Render)
-const productionConfig = {
-  dialect: 'postgres',
-  url: process.env.DB_URL_PROD,
-  dialectOptions: {
-    ssl: {
-      require: true,
-      rejectUnauthorized: false,
-    },
-  },
-  define: {
-    timestamps: true,
-    underscored: true,
-    underscoredAll: true,
-  },
-};
-
-module.exports = process.env.NODE_ENV === 'production' ? productionConfig : developmentConfig;
